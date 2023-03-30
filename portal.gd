8df25c57e4ffb05558ce896e784f0c9fdf784ed9
Nabil Ahammar <ahammar2003@gmail.com>
2023-03-30 10:05:16 +0200

    Working level system

--- portal.gd
+++ portal.gd
@@ -6,5 +6,11 @@ extends Area2D
 
 #scene switc hing switches scene directly and not when u enter... maybe change it tomorow
 func _on_body_entered(body):
-	get_tree().change_scene_to(load("res://level_1.tscn"))
+	var currentLevel = get_tree().get_current_scene().get_name()
+	if body.is_in_group("player"):
+		match (currentLevel):
+			"Level_0":
+				get_tree().change_scene_to_file("res://level_1.tscn")
+			"Level_1":
+				print ("No other levels yet")
 
