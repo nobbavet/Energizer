e74f0f4b970cb338d578a2f8ce19fbb04f5ff2e0
Nabil Ahammar <ahammar2003@gmail.com>
2023-03-30 10:04:22 +0200

    Failure portal code

--- portal.gd
+++ portal.gd
@@ -1,9 +1,10 @@
 extends Area2D
 
-export(String, FILE) var NEXT_LEVEL: String = ""
+#func _on_body_entered(_body):
+#	if body.is_in_group("player"):
+#		get_tree().change_scene("res://Level_" + str(int(get_tree().current_scene.name) + 1) + ".tscn")		
 
+#scene switc hing switches scene directly and not when u enter... maybe change it tomorow
+func _on_body_entered(body):
+	get_tree().change_scene_to(load("res://level_1.tscn"))
 
-func _on_portal_entered(body):
-
-
-edd]]====]
